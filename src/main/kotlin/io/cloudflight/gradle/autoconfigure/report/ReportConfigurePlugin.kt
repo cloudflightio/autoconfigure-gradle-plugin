71be30b336805244e82b21a55c1b1400de79a16f
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-06-15 09:58:03 +0200

    #51 configuring the ReportConfigurePlugin.kt in afterEvaluate in order to ensure that the JavaLibraryPlugin does not come later and again adds a TestSpec

--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/report/ReportConfigurePlugin.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/report/ReportConfigurePlugin.kt
@@ -14,20 +14,25 @@ open class ReportConfigurePlugin : Plugin<Project> {
         if (project != project.rootProject) throw GradleException("'report-configure' plugin can only be applied to the root project.")
         project.plugins.apply(JacocoReportAggregationPlugin::class.java)
 
-        if (project.subprojects.isNotEmpty()) {
-            val reporting = project.extensions.getByType(ReportingExtension::class.java)
-            if (!reporting.reports.any { it.name == REPORT_TASK_NAME }) {
-                reporting.reports.create(REPORT_TASK_NAME, JacocoCoverageReport::class.java) {
-                    it.testType.set(TestSuiteType.UNIT_TEST)
-                }
+        // we need to do this block in afterEvaluate as it can be that additional plugins are being applied
+        // in the build.gradle file at a later point of time which in turn apply the JavaLibraryPlugin and
+        // that wants to create its own test spec
+        project.afterEvaluate {
+            if (project.subprojects.isNotEmpty()) {
+                val reporting = project.extensions.getByType(ReportingExtension::class.java)
+                if (!reporting.reports.any { it.name == REPORT_TASK_NAME }) {
+                    reporting.reports.create(REPORT_TASK_NAME, JacocoCoverageReport::class.java) {
+                        it.testType.set(TestSuiteType.UNIT_TEST)
+                    }
 
-                for (subproject in project.subprojects) {
-                    project.dependencies.add(
-                        JacocoReportAggregationPlugin.JACOCO_AGGREGATION_CONFIGURATION_NAME,
-                        subproject
-                    )
+                    for (subproject in project.subprojects) {
+                        project.dependencies.add(
+                            JacocoReportAggregationPlugin.JACOCO_AGGREGATION_CONFIGURATION_NAME,
+                            subproject
+                        )
+                    }
                 }
-            }
+          }
         }
     }
 
