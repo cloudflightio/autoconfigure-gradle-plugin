39c3a743913290e4ff94e55938f6de05f062f965
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-08-09 22:52:33 +0200

    #79 auto-apply the test-fixture plugin if src/testFixtures exist

---
+++ src/test/fixtures/java/single-java-module-testfixtures/src/testFixtures/java/io/cloudflight/gradle/Person.java
@@ -0,0 +1,14 @@
+package io.cloudflight.gradle;
+
+public class Person {
+
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
