8e106f8aa56e4d0ebe4953ba659cd5ec0d9bcd81
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-08-22 12:05:16 +0200

    improved documentation
    Signed-off-by: Klaus Lehner <172195+klu2@users.noreply.github.com>

--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/java/JavaConfigurePluginExtension.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/java/JavaConfigurePluginExtension.kt
@@ -5,11 +5,40 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 abstract class JavaConfigurePluginExtension {
 
-    abstract val applicationBuild: Property<Boolean>
+    /**
+     * If `true`, the sourcesJar and javadoc will also be generated for this module.
+     * This default is `true`
+     */
     abstract val createSourceArtifacts: Property<Boolean>
+
+    /**
+     * If this property is true on the given module, we are applying additional plugins based on the setting of the
+     * property [applicationFramework].
+     *
+     * This is `true` per default for all modules which end with `-server`
+     */
+    abstract val applicationBuild: Property<Boolean>
+
+    /**
+     * Depending on the value of this property we apply the underlying framework plugin (the spring boot plugin for
+     * Spring Boot and the Shadow-Plugin for Micronaut)
+     */
     abstract val applicationFramework: Property<ApplicationFramework>
+
+    /**
+     * The version of JDK that is being used to compile Java and Kotlin code.
+     * This default is 17.
+     */
     abstract val languageVersion: Property<JavaLanguageVersion>
+
+    /**
+     * The encoding for all sourcesets (main and test), the default is `UTF-8`
+     */
     abstract val encoding: Property<String>
+
+    /**
+     * The `vendorName` is being used as `Implementation-Vendor` inside the `MANIFEST.MF`
+     */
     abstract val vendorName: Property<String>
 
 }
