ae8797158ee7c19c0308a287d8c29bb35f049e17
Kangrui Ye <kangrui.ye@cloudflight.io>
2022-10-06 09:08:23 +0200

    #91: allow to specify custom generator for node projects
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/swagger/SwaggerCodegenConfigurePluginExtension.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/swagger/SwaggerCodegenConfigurePluginExtension.kt
@@ -5,5 +5,6 @@ import org.gradle.api.provider.Property
 abstract class SwaggerCodegenConfigurePluginExtension {
 
     abstract val swaggerCodegenCliVersion: Property<String>
+    abstract val nodeSwaggerGenerator: Property<String>
 
 }
