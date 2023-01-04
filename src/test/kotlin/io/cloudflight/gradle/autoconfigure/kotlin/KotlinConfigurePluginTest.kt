c622ef636ffbfd06c6500f04e47873aac066827b
Klaus Lehner <172195+klu2@users.noreply.github.com>
2023-01-04 08:31:43 +0100

    #140 update to Kotlin 1.8.0 (#141)
    Signed-off-by: Klaus Lehner <172195+klu2@users.noreply.github.com>

--- src/test/kotlin/io/cloudflight/gradle/autoconfigure/kotlin/KotlinConfigurePluginTest.kt
+++ src/test/kotlin/io/cloudflight/gradle/autoconfigure/kotlin/KotlinConfigurePluginTest.kt
@@ -75,7 +75,7 @@ class KotlinConfigurePluginTest {
         @JvmStatic
         fun singleKotlinModuleArguments(): Stream<Arguments> {
             // keep in sync with the kotlin version in libs.versions.toml
-            val currentKotlinVersion = "1.7.21"
+            val currentKotlinVersion = "1.8.0"
             return Stream.of(
                 arguments(
                     TestOptions(
