b5b2c00faf623dc9c0b1279bbd28670d5dcf3f36
Clemens Grabmann <clemens.grabmann@cloudflight.io>
2023-08-18 14:20:20 +0200

    minor: (#126) recognize kotlin directory in all source sets and configure kotlin if found * do the same for java
    Signed-off-by: Clemens Grabmann <clemens.grabmann@cloudflight.io>

--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/kotlin/IsKotlinProject.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/kotlin/IsKotlinProject.kt
@@ -3,6 +3,8 @@ package io.cloudflight.gradle.autoconfigure.kotlin
 import org.gradle.api.Project
 
 internal fun isKotlinProject(project: Project): Boolean {
-    return project.layout.projectDirectory.dir("src/main/kotlin/").asFile.exists() ||
-            project.layout.projectDirectory.dir("src/test/kotlin/").asFile.exists()
+    val kotlinDirs = project.layout.projectDirectory.asFileTree.matching {
+        it.include("src/*/kotlin/")
+    }
+    return !kotlinDirs.isEmpty
 }
---
+++ src/test/fixtures/autoconfigure/single-java-module-with-kotlin-fixtures/src/test/java/io/cloudflight/gradle/FooTest.java
@@ -0,0 +1,9 @@
+
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+    @Test
+    public void test() {
+        new FooFixture();
+    }
+}
