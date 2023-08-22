acc5c9f5f934edb1592d15c8d148f63c5f3b5070
Clemens Grabmann <clemens.grabmann@cloudflight.io>
2023-08-22 16:08:02 +0200

    add workaround for lazy task configuration of the `generateOpenApiDocs` task
    Signed-off-by: Clemens Grabmann <clemens.grabmann@cloudflight.io>

--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/springdoc/openapi/SpringDocOpenApiConfigurePlugin.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/springdoc/openapi/SpringDocOpenApiConfigurePlugin.kt
@@ -2,8 +2,8 @@ package io.cloudflight.gradle.autoconfigure.springdoc.openapi
 
 import com.github.psxpaul.task.JavaExecFork
 import io.cloudflight.gradle.autoconfigure.AutoConfigureGradlePlugin.Companion.TASK_GROUP
-import io.cloudflight.gradle.autoconfigure.extentions.gradle.api.named
-import io.cloudflight.gradle.autoconfigure.extentions.gradle.api.withType
+import io.cloudflight.gradle.autoconfigure.extentions.gradle.api.tasks.named
+import io.cloudflight.gradle.autoconfigure.extentions.gradle.api.tasks.withType
 import io.cloudflight.gradle.autoconfigure.java.JavaConfigurePlugin
 import io.cloudflight.gradle.autoconfigure.util.addApiDocumentationPublication
 import org.gradle.api.Plugin
@@ -14,6 +14,7 @@ import org.gradle.api.tasks.TaskProvider
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springdoc.openapi.gradle.plugin.OpenApiExtension
+import org.springdoc.openapi.gradle.plugin.OpenApiGeneratorTask
 import org.springdoc.openapi.gradle.plugin.OpenApiGradlePlugin
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 import java.net.ServerSocket
@@ -29,7 +30,8 @@ class SpringDocOpenApiConfigurePlugin : Plugin<Project> {
         extension.fileFormat.convention(OpenApiFormat.YAML);
         val openapi = target.extensions.getByType(OpenApiExtension::class.java)
         configureOpenApiExtension(openapi, extension, target, target.name)
-        val openApiTask = target.tasks.named("generateOpenApiDocs")
+        val openApiTask = target.tasks.named("generateOpenApiDocs", OpenApiGeneratorTask::class)
+        makeOpenApiTaskReactive(openApiTask, openapi)
 
         val documentationTask = target.tasks.register("clfGenerateOpenApiDocumentation") {
             it.group = TASK_GROUP
@@ -45,6 +47,23 @@ class SpringDocOpenApiConfigurePlugin : Plugin<Project> {
         target.afterEvaluate {
             configureDocumentPublishing(openapi, target, openApiTask)
         }
+    }
+
+    private fun makeOpenApiTaskReactive(openApiTask: TaskProvider<OpenApiGeneratorTask>, openapi: OpenApiExtension) {
+        // for some reason the springdoc plugin reads the values from the extension during task initialization and uses that as convention for the task properties,
+        // which results in some values being incorrect. Because of that we reconfigure the properties conventions to directly use the extension properties. And
+        // add conventions to the extension properties to the expected default values see: https://github.com/springdoc/springdoc-openapi-gradle-plugin/blob/master/src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiGeneratorTask.kt#L46
+        openApiTask.configure {
+            it.apiDocsUrl.convention(openapi.apiDocsUrl)
+            it.outputFileName.convention(openapi.outputFileName)
+            it.groupedApiMappings.convention(openapi.groupedApiMappings)
+            it.outputDir.convention(openapi.outputDir)
+        }
+
+        openapi.apiDocsUrl.convention("http://localhost:8080/v3/api-docs")
+        openapi.outputFileName.convention("openapi.json")
+        openapi.groupedApiMappings.convention(emptyMap())
+        openapi.outputDir.convention(openApiTask.flatMap { it.project.layout.buildDirectory })
     }
 
     private fun `setupWorkaroundFor#171`(target: Project, openapi: OpenApiExtension) {
@@ -80,30 +99,30 @@ class SpringDocOpenApiConfigurePlugin : Plugin<Project> {
         target: Project,
         basename: String
     ) {
-        with(openapi) {
-            val serverPort = freeServerSocketPort()
-            val managementPort = freeServerSocketPort()
-            val docsUrl = configureExtension.fileFormat.map {
-                val basePath = "http://localhost:${serverPort}/v3/api-docs"
-                when (it) {
-                    OpenApiFormat.JSON -> basePath
-                    else -> "${basePath}.${it.extension}"
-                }
+        val serverPort = freeServerSocketPort()
+        val managementPort = freeServerSocketPort()
+        val outputFileName = configureExtension.fileFormat.map { "${basename}.${it.extension}" }
+        val docsUrl = openapi.outputFileName.map {
+            val basePath = "http://localhost:${serverPort}/v3/api-docs"
+            when {
+                it.endsWith(".${OpenApiFormat.JSON.extension}") -> basePath
+                it.endsWith(".${OpenApiFormat.YAML.extension}") -> "${basePath}.${OpenApiFormat.YAML.extension}"
+                else -> throw UnsupportedFormatException("The provided openapi filename '${it}' ends in an unsupported extension. Make sure you use 'yaml' or 'json'")
             }
+        }
 
-            outputDir.set(target.layout.buildDirectory.dir("generated/resources/openapi"))
-            outputFileName.set(configureExtension.fileFormat.map { "${basename}.${it.extension}" })
-            apiDocsUrl.set(docsUrl)
-            customBootRun {
-                it.workingDir.set(target.layout.buildDirectory.dir("dummyForkedSpringBootWorkingDir"))
-            }
+        openapi.outputDir.set(target.layout.buildDirectory.dir("generated/resources/openapi"))
+        openapi.outputFileName.set(outputFileName)
+        openapi.apiDocsUrl.set(docsUrl)
+        openapi.customBootRun {
+            it.workingDir.set(target.layout.buildDirectory.dir("dummyForkedSpringBootWorkingDir"))
+        }
 
-            mapOf(
-                "--server.port" to serverPort,
-                "--management.server.port" to managementPort
-            ).forEach { arg ->
-                customBootRun.args.add("${arg.key}=${arg.value}")
-            }
+        mapOf(
+            "--server.port" to serverPort,
+            "--management.server.port" to managementPort
+        ).forEach { arg ->
+            openapi.customBootRun.args.add("${arg.key}=${arg.value}")
         }
     }
 
@@ -117,7 +136,7 @@ class SpringDocOpenApiConfigurePlugin : Plugin<Project> {
     private fun configureDocumentPublishing(
         openapi: OpenApiExtension,
         target: Project,
-        task: TaskProvider<Task>,
+        task: TaskProvider<out Task>,
     ) {
         val format = openapi.outputFileName.map {
             if (it.endsWith(".${OpenApiFormat.YAML.extension}")) {
