bed88bc7fb78ed7708165e1a016ad0749d009365
klehner <172195+klu2@users.noreply.github.com>
2023-08-28 09:14:44 +0200

    minor: update to Node Plugin 7.0.0, have a default of Node 18.17.1 and NPM 9.6.7
    Signed-off-by: klehner <172195+klu2@users.noreply.github.com>

--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/node/Defaults.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/node/Defaults.kt
@@ -1,6 +1,5 @@
 package io.cloudflight.gradle.autoconfigure.node
 
-const val NODE_VERSION = "18.16.1"
+const val NODE_VERSION = "18.17.1"
 
-const val NPM_VERSION = "8.19.2"
-const val YARN_VERSION = "3.2.4"
+const val NPM_VERSION = "9.6.7"
--- src/main/kotlin/io/cloudflight/gradle/autoconfigure/node/NodeConfigurePluginExtension.kt
+++ src/main/kotlin/io/cloudflight/gradle/autoconfigure/node/NodeConfigurePluginExtension.kt
@@ -1,40 +1,17 @@
 package io.cloudflight.gradle.autoconfigure.node
 
-import org.gradle.api.Action
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import java.io.File
-import javax.inject.Inject
 
-abstract class NodeConfigurePluginExtension @Inject constructor(
-    objects: ObjectFactory
-) {
+abstract class NodeConfigurePluginExtension {
 
     abstract val nodeVersion: Property<String>
     abstract val downloadNode: Property<Boolean>
 
-    val npm: NpmConfigurePluginExtension = objects.newInstance(NpmConfigurePluginExtension::class.java)
-    fun npm(action: Action<NpmConfigurePluginExtension>) {
-        action.execute(npm)
-    }
-
     abstract val npmVersion: Property<String>
 
     abstract val destinationDir: Property<File>
-    abstract val inputFiles: ListProperty<Any>
-
-}
-
-abstract class NpmConfigurePluginExtension {
-
-    @Deprecated("use node.npmVersion instead")
-    abstract val npmVersion: Property<String>
-
-    @Deprecated("use node.destinationDir instead")
-    abstract val destinationDir: Property<File>
-
-    @Deprecated("use node.inputFiles instead")
     abstract val inputFiles: ListProperty<Any>
 
 }
